b2577dd507d0c4c8ee95f4c5baad08459568480e
Ameer Hamza <ameer.hamza102002@gmail.com>
2023-07-10 13:45:40 +0500

    updated

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const express=require('express');
 const cors=require('cors');
 const dotenv=require('dotenv');
 const productRoutes =require('./routes/productRoutes');
+const bodyParser=require('body-parser');
 
 dotenv.config();
 
 const app=express();
 
 app.use(cors());
-app.use(express());
+app.use(express.json());
+app.use(bodyParser.json());
 app.use('/products',productRoutes);
 
 app.listen(process.env.APP_PORT,()=>{
